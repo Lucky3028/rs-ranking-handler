8483c06b543ecd049a6d4afef3af80fc7d8e1677
Lucky <lucky3028@users.noreply.github.com>
2021-07-13 14:47:56 +0900

    [Add] APIデータを扱いやすくするための型

--- src/seichi_api.rs
+++ src/seichi_api.rs
@@ -32,3 +32,31 @@ pub struct Rankings {
 pub async fn deserialize(response: reqwest::Response) -> Result<Rankings, Box<(dyn error::Error)>> {
     Ok(response.json::<Rankings>().await?)
 }
+
+#[derive(Clone, Debug)]
+pub struct Lottery {
+    pub ranking_type: String,
+    pub rank: u32,
+    pub amount: String,
+    pub player_name: String,
+}
+
+impl Lottery {
+    fn new(
+        ranking_type: impl Into<String>,
+        rank: u32,
+        amount: impl Into<String>,
+        player_name: impl Into<String>,
+    ) -> Lottery {
+        Lottery {
+            ranking_type: ranking_type.into(),
+            rank,
+            amount: amount.into(),
+            player_name: player_name.into(),
+        }
+    }
+
+    pub fn convert(rank: &Rank) -> Lottery {
+        Lottery::new(&rank.r_type, rank.rank, &rank.amount.raw_data, &rank.player.name)
+    }
+}
