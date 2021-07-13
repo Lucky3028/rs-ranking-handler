d5253837441b0b5dacbcbdb00dfecf41e8083535
Lucky <lucky3028@users.noreply.github.com>
2021-07-13 15:25:49 +0900

    [Add] 処理をまとめて整形

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use ranking_handler::{fetch, ranking_type, seichi_api};
-use std::{collections::HashMap, process};
 use rand::seq::SliceRandom;
+use ranking_handler::{fetch, ranking_type::RankingType, seichi_api, util};
+use std::{collections::HashMap, process};
 
 const RANKING_URL: &str = "https://ranking-gigantic.seichi.click/api/ranking";
 
-fn queries(ranking_type: ranking_type::RankingType) -> HashMap<String, String> {
+fn queries(ranking_type: RankingType) -> HashMap<String, String> {
     let mut map = HashMap::new();
     map.insert("type".to_string(), ranking_type.as_str());
     map.insert("offset".to_string(), "0".to_string());
@@ -13,44 +13,73 @@ fn queries(ranking_type: ranking_type::RankingType) -> HashMap<String, String> {
     map
 }
 
-#[tokio::main]
-async fn main() {
-    let result = fetch::fetch(RANKING_URL, Some(queries(ranking_type::RankingType::Break))).await;
+async fn fetch_data(ranking_type: RankingType) -> Vec<seichi_api::Lottery> {
+    let result = fetch::fetch(RANKING_URL, Some(queries(ranking_type))).await;
     if let Err(e) = result {
-        eprintln!("{}", e);
+        eprintln!(r"APIとの通信中にエラーが発生しました。\n{}", e);
         process::exit(1);
     }
     let result = seichi_api::deserialize(result.unwrap()).await;
     if let Err(e) = result {
-        eprintln!("{}", e);
+        eprintln!(r"型変換の実行中にエラーが発生しました。\n{}", e);
         process::exit(1);
     }
     let result = result.unwrap().ranks;
-    let result: Vec<_> = result.iter().map(|rank| seichi_api::Lottery::convert(rank)).collect();
+    result
+        .iter()
+        .map(|rank| seichi_api::Lottery::convert(rank))
+        .collect()
+}
+
+#[tokio::main]
+async fn main() {
+    println!("月別ランキング報酬の抽選を開始します。");
+    println!();
+
+    println!("今月の月別景品付与の対象になったプレイヤーの方々を一覧表示します。");
+    util::pause();
+
+    println!();
+
+    println!("整地量：20名");
+    println!();
+    let break_targets = fetch_data(RankingType::Break).await;
+    println!("{:#?}", break_targets);
+
+    println!();
+
+    println!("建築量：10名");
+    println!();
+    let build_targets = fetch_data(RankingType::Build).await;
+    println!("{:#?}", build_targets);
+
+    println!();
+
+    println!("景品が実際に付与される方はこちらです。");
+    util::pause();
+
+    println!();
+
+    println!("整地量：5名／20名");
+    println!();
     let rng = &mut rand::thread_rng();
-    let _samples: Vec<_> = result.choose_multiple(rng, 1).cloned().collect();
-    println!("{:#?}", _samples);
+    let break_winner: Vec<_> = break_targets
+        .choose_multiple(rng, RankingType::Break.get_winners().into())
+        .cloned()
+        .collect();
+    println!("{:#?}", break_winner);
 
-    // println!("月別ランキング報酬の抽選を開始します。");
-    // println!();
+    println!();
 
-    // println!("今月の月別景品付与の対象になったプレイヤーの方々を一覧表示します。");
-    // util::pause();
+    println!("建築量：3名／10名");
+    println!();
+    let build_winner: Vec<_> = build_targets
+        .choose_multiple(rng, RankingType::Build.get_winners().into())
+        .cloned()
+        .collect();
+    println!("{:#?}", build_winner);
 
-    // println!("整地量：20名");
-    // println!();
+    println!();
 
-    // println!("建築量：10名");
-    // println!();
-
-    // println!("景品が実際に付与される方はこちらです。");
-    // util::pause();
-
-    // println!("整地量：5名／20名");
-    // println!();
-
-    // println!("建築量：3名／10名");
-    // println!();
-
-    // println!("抽選を終了しました。");
+    println!("抽選を終了しました。");
 }
