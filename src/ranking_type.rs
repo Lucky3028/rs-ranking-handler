98a1f17d09f6450da0bb183e4a16115a22370037
Lucky <lucky3028@users.noreply.github.com>
2021-07-13 14:46:54 +0900

    [Add] RankingType

---
+++ src/ranking_type.rs
@@ -0,0 +1,27 @@
+pub enum RankingType {
+    Break,
+    Build,
+}
+
+impl RankingType {
+    pub fn as_str(&self) -> String {
+        match self {
+            RankingType::Break => "break",
+            RankingType::Build => "build"
+        }.to_string()
+    }
+
+    pub fn get_targets(&self) -> u8 {
+        match self {
+            RankingType::Break => 5,
+            RankingType::Build => 10
+        }
+    }
+
+    pub fn get_winners(&self) -> u8 {
+        match self {
+            RankingType::Break => 3,
+            RankingType::Build => 2
+        }
+    }
+}
