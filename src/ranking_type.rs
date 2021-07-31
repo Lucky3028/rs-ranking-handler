7c024818a114227dd8472420dfec4da5802605db
Lucky <lucky3028@users.noreply.github.com>
2021-07-31 23:09:51 +0900

    [Fix] 当選者数が間違えていたので修正

--- src/ranking_type.rs
+++ src/ranking_type.rs
@@ -20,8 +20,8 @@ impl RankingType {
 
     pub fn get_winners(&self) -> u8 {
         match self {
-            RankingType::Break => 5,
-            RankingType::Build => 3
+            RankingType::Break => 3,
+            RankingType::Build => 2
         }
     }
 }
