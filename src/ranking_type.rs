3104ff9659532e21f85000fb2132743b7805c608
lucky <lucky3028@users.noreply.github.com>
2021-10-26 22:09:24 +0900

    [Fix] strumを使って定数をまとめる

--- src/ranking_type.rs
+++ src/ranking_type.rs
@@ -1,27 +1,26 @@
+use std::{convert::AsRef, str::FromStr};
+
+use strum::{AsRefStr, EnumProperty};
+
+#[derive(AsRefStr, EnumProperty)]
 pub enum RankingType {
+    #[strum(props(targets = "20", winners = "3"))]
     Break,
+    #[strum(props(targets = "10", winners = "2"))]
     Build,
 }
 
 impl RankingType {
     pub fn as_str(&self) -> String {
-        match self {
-            RankingType::Break => "break",
-            RankingType::Build => "build"
-        }.to_string()
+        let str: &str = self.as_ref();
+        str.to_lowercase()
     }
 
     pub fn get_targets(&self) -> u8 {
-        match self {
-            RankingType::Break => 20,
-            RankingType::Build => 10
-        }
+        self.get_str("targets").and_then(|str| u8::from_str(str).ok()).unwrap_or(0)
     }
 
     pub fn get_winners(&self) -> u8 {
-        match self {
-            RankingType::Break => 3,
-            RankingType::Build => 2
-        }
+        self.get_str("winners").and_then(|str| u8::from_str(str).ok()).unwrap_or(0)
     }
 }
