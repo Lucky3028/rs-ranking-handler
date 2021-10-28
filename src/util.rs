c47655a388bbe93b7d9e084cdc1d99fb586d9cea
lucky <lucky3028@users.noreply.github.com>
2021-10-28 15:17:47 +0900

    [Remove] 不要になった関数

--- src/util.rs
+++ src/util.rs
@@ -8,14 +8,10 @@ pub fn pause() {
     io::stdin().read_exact(&mut [0u8]).unwrap();
 }
 
-pub async fn deserialize(response: reqwest::Response) -> Result<Rankings, Box<(dyn std::error::Error)>> {
-    Ok(response.json::<Rankings>().await?)
-}
-
 pub async fn fetch(
     url: &str,
     query: Option<HashMap<String, String>>,
-) -> Result<reqwest::Response, Box<dyn std::error::Error>> {
+) -> anyhow::Result<reqwest::Response> {
     let response = reqwest::Client::new()
         .get(url)
         .query(&query.unwrap_or_default())
